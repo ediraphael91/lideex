55aebb25f95c14b17261eb96fc1a8b19d10be549
ediraphael91 <ediraphael91@gmail.com>
2025-10-03 15:19:11 -0500

    Se agrega atribuos a la tabla user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('apellido');
+            $table->string('cedula')->unique();
             $table->string('email')->unique();
+            $table->string('telefono')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
